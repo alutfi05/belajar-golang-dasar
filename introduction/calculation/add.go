88768fe1a4ad26eedfe204082ea96befbe161243
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-08-02 16:08:03 +0700

    learning about package

---
+++ introduction/calculation/add.go
@@ -0,0 +1,8 @@
+package calculation
+
+// Noted
+// Function harus menggunakan huruf capital agar bisa di akses dari file lain
+
+func Add(numberOne int, numberTwo int) int {
+	return numberOne + numberTwo
+}
--- introduction/main.go
+++ introduction/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+// Noted
+// Cara import : import "namaModul/namaPackage", contoh -> import "introduction/calculation"
+// Cara pemakaian : calculation.Add(1, 2)
+
+import (
+	"fmt"
+	"introduction/calculation"
+)
 
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println("Halo, belajar golang")
+
+	result := calculation.Add(1,2)
+	fmt.Println(result)
 }
