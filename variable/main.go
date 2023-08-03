24c7ddb0233d1808f0a9d5b1ffba54727018d4ae
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-08-03 12:35:57 +0700

    learning how to create variable in golang

---
+++ variable/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string
+	firstName = "Ahmad Lutfi"
+	fmt.Println(firstName)
+
+	var lastName string = "Rizki Patria"
+	fmt.Println(lastName)
+
+	fullName := "Ahmad Lutfi Rizki Patria"
+	fmt.Println(fullName)
+}
