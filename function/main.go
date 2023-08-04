2ad86f7c8e7b42e11d2bceb394635f6ef4949c36
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-08-04 15:58:35 +0700

    function advanced

--- function/main.go
+++ function/main.go
@@ -5,9 +5,16 @@ import "fmt"
 func main() {
 	sentence := printMyResult("Saya sedang")
 	fmt.Println(sentence)
+
+	result := add(5, 2)
+	fmt.Println(result)
 }
 
 func printMyResult(sentence string) string {
 	newSentence := sentence + " belajar golang"
 	return newSentence
+}
+
+func add(numberOne, numberTwo int) int {
+	return numberOne + numberTwo
 }
