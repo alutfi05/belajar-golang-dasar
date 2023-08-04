67c33cf6258bc0cac50e686b0ca539b65a8c0621
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-08-04 16:04:34 +0700

    function with multiple return

--- function/main.go
+++ function/main.go
@@ -8,13 +8,30 @@ func main() {
 
 	result := add(5, 2)
 	fmt.Println(result)
+
+	luas, keliling := rectangle(5, 2);
+	fmt.Println("Luas :", luas)
+	fmt.Println("Keliling :", keliling)
+	
+	// luas, _ := rectangle(5, 2);
+	// fmt.Println("Luas :", luas)
 }
 
+// Basic function
 func printMyResult(sentence string) string {
 	newSentence := sentence + " belajar golang"
 	return newSentence
 }
 
+// Advanced function
 func add(numberOne, numberTwo int) int {
 	return numberOne + numberTwo
+}
+
+// Function with multiple return
+func rectangle(panjang, lebar int) (int, int) {
+	luas := panjang * lebar
+	keliling := 2 * (panjang + lebar)
+
+	return luas, keliling
 }
