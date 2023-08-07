ce9b421a89c81d2e1404b7f2f2e9186e51a4e111
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-08-07 17:16:25 +0700

    exported and unexported package

--- struct/main.go
+++ struct/main.go
@@ -1,83 +1,36 @@
 package main
 
-import "fmt"
-
-type User struct {
-	ID        int
-	FirstName string
-	LastName  string
-	Email     string
-	isActive  bool
-}
-
-// Method User Struct
-func (user User) display() string {
-	return fmt.Sprintf("Name: %s %s, Email: %s", user.FirstName, user.LastName, user.Email)
-}
-
-// Embedded Struct
-type Group struct {
-	Name 		string
-	Admin 		User
-	Users 		[]User
-	isAvailable bool
-}
+import (
+	"fmt"
+	"struct/management"
+)
 
 func main() {
-	user := User{}
+	user := management.User{}
 	user.ID = 1
 	user.FirstName = "Ahmad Lutfi"
 	user.LastName = "Rizki Patria"
 	user.Email = "aluthfi0522@gmail.com"
-	user.isActive = true
+	user.IsActive = true
 
-	user2 := User{
+	fmt.Println(user.Display())
+
+	user2 := management.User{
 		ID: 2,
 		FirstName: "Laila Indah", 
 		LastName: "Agustin", 
 		Email: "lailaaa1818@gmail.com", 
-		isActive: true,
+		IsActive: true,
 	}
 
-	displayUser1 := displayUser(user)
-	displayUser2 := displayUser(user2)
+	users := []management.User{user, user2}
 
-	fmt.Println(displayUser1)
-	fmt.Println(displayUser2)
-
-	// Embedded Struct
-	users := []User{user, user2}
-
-	group := Group{
+	group := management.Group{
 		Name: "Gaming",
 		Admin: user,
 		Users: users,
-		isAvailable: true,
+		IsAvailable: true,
 	}
 
-	displayGroup(group)
-
-	// Method Struct
-	resultDisplay1 := user.display()
-	resultDisplay2 := user2.display()
-
-	fmt.Println(resultDisplay1)
-	fmt.Println(resultDisplay2)
-}
-
-// Struct as parameter 
-func displayUser(user User) string {
-	return fmt.Sprintf("Name: %s %s, Email: %s", user.FirstName, user.LastName, user.Email)
-}
-
-func displayGroup(group Group) {
-	fmt.Printf("Name: %s", group.Name)
-	fmt.Println("")
-	fmt.Printf("Member count: %d", len(group.Users))
-	fmt.Println("")
-
-	fmt.Println("Users name")
-	for _, user := range group.Users {
-		fmt.Println(user.FirstName)
-	}
+	group.DisplayGroup()
 }
